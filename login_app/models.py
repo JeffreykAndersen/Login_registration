adc325e1cbff891af02b9b91cd656e372edeceb6
Jeffrey Andersen <jeffreykandersen@gmail.com>
2020-07-15 22:58:37 -0700

    inital commit

---
+++ login_app/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+import re 
+import bcrypt
+#regex import
+
+#Pass in a pattern that email must match from the imported re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$') #regex email format
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        # Figure pout how to validate first and last name to have no numbers
+        # nonum = str.isalpha() 
+        if len(postData['first_name'])< 2:
+            errors['first_name'] = "First Name is Required."
+        if len(postData['last_name'])< 2:
+            errors['last_name'] = "Last Name is Required."
+        # if len(postData['email'])< 1:
+        #     errors['email'] = "Email is required."
+        if not EMAIL_REGEX.match(postData['email']):    # test whether a field matches the pattern            
+            errors['email'] = "Invalid email address!"
+        if len(postData['password'])< 8:
+            errors['password'] = "Password must be at least 8 characters."
+        if postData['password'] != postData['confirm']: #Check to make sure password and confirm are equal
+            errors['confirm'] = "Passwords do not match."
+        #prevent Duplicate emails
+        users_with_email = self.filter(email=postData['email'])
+        if users_with_email:
+            errors['email'] = "Email is already in use."
+        return errors
+    
+    #place creation in models instead so views stays minimal
+    def register(self, postData):
+        hashed_password = bcrypt.hashpw(postData['password'].encode(),bcrypt.gensalt()).decode()
+        User.objects.create(
+            first_name = postData['first_name'],
+            last_name = postData['last_name'],
+            email = postData['email'],
+            password = hashed_password
+        )
+    
+    def authenticate(self, email, password):
+        #return true/false
+        users_with_email = self.filter(email=email)
+        if not users_with_email:
+            return False
+        user = users_with_email[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+
+
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+    update_on = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
---
+++ login_app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+from .models import User
+import bcrypt
+
+def home(request):
+    return render(request, "main.html")
+
+def register(request):
+    if request.method =="POST":
+        errors = User.objects.basic_validator(request.POST)
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+                print(key, value)
+            return redirect('/')
+        else:
+            #this calls my UserManager class to create and register my user. Skinny views Fat Models.
+            User.objects.register(request.POST)
+            log_user = User.objects.last()
+            request.session['user'] = f"{log_user.first_name} {log_user.last_name}"
+            request.session['email'] = log_user.email
+            return redirect('/success')
+
+def login(request):
+    result = User.objects.authenticate(request.POST['log_email'], request.POST['log_password'])
+    if result == False:
+        messages.error(request, "Invalid Credentials")
+        return redirect('/')
+    if result == True:
+        log_user = User.objects.get(email=request.POST['log_email'])
+        request.session['user'] = f"{log_user.first_name} {log_user.last_name}"
+        request.session['email'] = log_user.email
+        return redirect('/success')
+
+def success(request):
+    if 'user' not in request.session:
+        return redirect('/')
+    return render(request, "loggedin.html",)
+
+ 
+def logout(request):
+    request.session.flush()
+    print("session flushed")
+    return redirect('/')
+
+
+
+
